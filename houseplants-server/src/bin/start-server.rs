11adc572ede3c5a14bf4447475ffb466ce675057
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-03 13:09:11 +0200

    fixing: move the folders and files (server modules) to the right place unser the 'houseplant-server' folder

---
+++ houseplants-server/src/bin/start-server.rs
@@ -0,0 +1,26 @@
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::postgres::PgPool;
+use std::env;
+use std::io;
+
+#[path = "../server_modules/db_access/mod.rs"]
+mod dbaccess;
+#[path = "../server_modules/handlers/mod.rs"]
+mod handlers;
+#[path = "../server_modules/models/mod.rs"]
+mod models;
+#[path = "../server_modules/routes.rs"]
+mod routes;
+#[path = "../server_modules/state.rs"]
+mod state;
+#[path = "../server_modules/errors.rs"]
+mod errors;
+
+// entry point to start server 
+
+fn main() {
+  println!("Hello, world!");
+}
+
+
