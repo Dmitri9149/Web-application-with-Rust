ff72914194644e7538c07850b9b318fc14d46c91
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-03 17:05:49 +0200

    add comments

--- houseplants-server/src/server_modules/routes.rs
+++ houseplants-server/src/server_modules/routes.rs
@@ -1,6 +1,7 @@
 use crate::handlers::{home::*, general::*};
 use actix_web::web;
 
+// home page route
 pub fn home_routes(cfg: &mut web::ServiceConfig) {
   cfg.service(
     web::scope("/home")
@@ -9,6 +10,7 @@ pub fn home_routes(cfg: &mut web::ServiceConfig) {
   );
 }
 
+// general route to test some functionalities 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
   cfg.route("general", web::get().to(general_page_handler));
 }
