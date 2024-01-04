87f938d73d9796f95c120ddaf39af952f05d9e51
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-04 16:02:28 +0200

    remove (currently) the update members route

--- houseplants-server/src/server_modules/routes.rs
+++ houseplants-server/src/server_modules/routes.rs
@@ -22,6 +22,7 @@ pub fn member_routes(cfg: &mut web::ServiceConfig) {
     .route("/", web::post().to(post_new_member))
     .route("/", web::get().to(get_members))
     .route("/{member_id}", web::delete().to(delete_member))
+//    .route("/{member_id}", web::put().to(update_member_details))
   );
 }
 
@@ -32,7 +33,7 @@ pub fn plant_routes(cfg: &mut web::ServiceConfig) {
   .route("/", web::post().to(post_new_plant))
   .route("/{member_id}", web::get().to(get_plants_for_member))
 //  .route("/{member_id}/{plant_id}", web::get().to(get_plant_details))
-  .route("/{member_id}/{plant_id}", web::put().to(update_plant_details))
+//  .route("/{member_id}/{plant_id}", web::put().to(update_plant_details))
   .route("/{member_id}/{plant_id}", web::delete().to(delete_plant))
   );
 }
