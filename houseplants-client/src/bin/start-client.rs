2f97ef8925ed39705a40c36da8f00fe0a29f7dbe
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-05 13:15:48 +0200

    fixing

--- houseplants-client/src/bin/start-client.rs
+++ houseplants-client/src/bin/start-client.rs
@@ -31,13 +31,14 @@ async fn main() -> std::io::Result<()> {
     .expect("Is DATABASE_URL set in .env file? From what folder you start server (where in .env file)?" );
   let db_pool = PgPool::connect(&db_url).await.unwrap();
 
-  // Construct App 
-  let app = move || {
     // server state as data 
     let shared_data = web::Data::new(AppState {
       server_is_running_message: "The server test page is running".to_string(),
       db: db_pool, 
     });
+
+  // Construct App 
+  let app = move || {
     App::new()
       .app_data(shared_data.clone())
       .configure(home_routes)
