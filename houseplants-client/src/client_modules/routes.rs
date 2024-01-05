0bddbeaee8d031fe07d21ad087bc5266068e7ebc
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-05 19:44:34 +0200

    mke some changes in route paths

--- houseplants-client/src/client_modules/routes.rs
+++ houseplants-client/src/client_modules/routes.rs
@@ -9,10 +9,8 @@ use actix_web::web;
 pub fn home_routes(cfg: &mut web::ServiceConfig) {
   cfg.service(
     web::scope("/home")
-    .service(fs::Files::new(
-      "/static", "./static").show_files_listing())
-    .route("", web::get().to(index))
-    .route("/{name}", web::get().to(hello))
+      .route("", web::get().to(index))
+      .route("/{name}", web::get().to(hello))
   );
 }
 
@@ -25,12 +23,12 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
 pub fn authorization_routes(config: &mut web::ServiceConfig) {
   config.service(
   web::scope("")
-  .service(fs::Files::new(
-    "/static", "./static").show_files_listing())
-  .service(web::resource("/").route(web::get().to(show_register_form)))
-  .service(web::resource("/signinform").route(web::get().to(show_signin_form)))
-  .service(web::resource("/signin").route(web::post().to(handle_signin)))
-  .service(web::resource("/register").route(web::post().to(handle_register)))
+    .service(fs::Files::new(
+      "/static", "./static").show_files_listing())
+    .service(web::resource("/").route(web::get().to(show_register_form)))
+    .service(web::resource("/signinform").route(web::get().to(show_signin_form)))
+    .service(web::resource("/signin").route(web::post().to(handle_signin)))
+    .service(web::resource("/register").route(web::post().to(handle_register)))
   );
 }
 
