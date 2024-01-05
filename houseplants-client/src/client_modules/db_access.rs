3bc7b49f395f8dab4053fbb1c47d1c5e94f159a2
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-05 15:52:58 +0200

    add get_user_record_db function

---
+++ houseplants-client/src/client_modules/db_access.rs
@@ -0,0 +1,23 @@
+use crate::errors::CustomError;
+use crate::model::*;
+use sqlx::postgres::PgPool;
+
+//Query user record from DB 
+pub async fn get_user_record(pool: &PgPool, username: String) -> 
+  Result<User, CustomError> {
+  // Prepare SQL statement
+  let result = sqlx::query_as!(
+    User,
+    "SELECT * FROM web_user WHERE username = $1",
+    username  
+  )
+  .fetch_optional(pool)
+  .await?;
+
+  if let Some(user) = result {
+    Ok(user)
+  } else {
+    Err(CustomError::NotFound("Username not found".into()))
+  }
+
+}
