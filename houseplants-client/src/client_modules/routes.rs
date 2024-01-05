7cf00a5b07ecfd62f733aab3b936b6c06eda0e50
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-05 16:30:35 +0200

    add the route to handle_signin (post)

--- houseplants-client/src/client_modules/routes.rs
+++ houseplants-client/src/client_modules/routes.rs
@@ -27,7 +27,7 @@ pub fn authorization(config: &mut web::ServiceConfig) {
     "/static", "./static").show_files_listing())
   .service(web::resource("/").route(web::get().to(show_register_form)))
   .service(web::resource("/signinform").route(web::get().to(show_signin_form)))
-//  .service(web::resource("/signin").route(web::post().to(handle_signin)))
+  .service(web::resource("/signin").route(web::post().to(handle_signin)))
   .service(web::resource("/register").route(web::post().to(handle_register)))
   );
 }
