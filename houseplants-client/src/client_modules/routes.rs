a6bcdbb8dbc5c7fd94e95f46c08eb48c1237dd10
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-07 19:53:46 +0200

    change scope definition and change correspondingly paths in templates

--- houseplants-client/src/client_modules/routes.rs
+++ houseplants-client/src/client_modules/routes.rs
@@ -8,7 +8,7 @@ use actix_web::web;
 // home page route
 pub fn home_routes(cfg: &mut web::ServiceConfig) {
   cfg.service(
-    web::scope("home")
+    web::scope("/home")
       .route("", web::get().to(index))
       .route("/{name}", web::get().to(hello))
   );
@@ -16,23 +16,24 @@ pub fn home_routes(cfg: &mut web::ServiceConfig) {
 
 // general route to test some functionalities 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
-  cfg.route("general", web::get().to(general_page_handler));
+  cfg.route("/general", web::get().to(general_page_handler));
 }
 
+/*
 // user authorization routes 
 pub fn templates_routes(config: &mut web::ServiceConfig) {
   config.service(
   web::scope("")
     .service(fs::Files::new(
       "/templates", "./templates").show_files_listing()));
-}
+} */
 
 // user authorization routes 
 pub fn authorization_routes(config: &mut web::ServiceConfig) {
   config.service(
-  web::scope("auth")
-/*    .service(fs::Files::new(
-      "/templates", "./templates").show_files_listing()) */
+  web::scope("")
+    .service(fs::Files::new(
+      "/templates", "./templates").show_files_listing())
     .service(web::resource("/").route(web::get().to(show_register_form)))
     .service(web::resource("/signinform").route(web::get().to(show_signin_form)))
     .service(web::resource("/signin").route(web::post().to(handle_signin)))
@@ -44,7 +45,7 @@ pub fn authorization_routes(config: &mut web::ServiceConfig) {
 // create / delete / modify plants records from the client 
 pub fn plant_routes(config: &mut web::ServiceConfig) {
   config.service(
-    web::scope("plants")
+    web::scope("/plants")
 //          .service(
 //              web::resource("show/{member_id}/{plant_id}").route(web::get().to(handle_show_member_plant)))
       .service(web::resource("/").route(web::get().to(show_new_plant_form)))
