91e9dadd8eb16c6c0f44c95f9214a8b3db63f3e6
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-08 15:01:04 +0200

    change authorization and plants roots

--- houseplants-client/src/client_modules/routes.rs
+++ houseplants-client/src/client_modules/routes.rs
@@ -10,21 +10,23 @@ use actix_web::web;
 pub fn home_routes(cfg: &mut web::ServiceConfig) {
   cfg.service(
     web::scope("/home")
-      .route("", web::get().to(index))
+      .route("/", web::get().to(index))
       .route("/{name}", web::get().to(hello))
   );
 }
 
 // general route to test some functionalities 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
-  cfg.route("/general", web::get().to(general_page_handler));
+  cfg.service(
+    web::scope("/general").route("/", web::get().to(general_page_handler))
+  );
 }
 
 
 // user authorization routes 
 pub fn authorization_routes(config: &mut web::ServiceConfig) {
   config.service(
-  web::scope("")
+  web::scope("/auth")
     .service(fs::Files::new(
       "/templates", "./templates").show_files_listing())
     .service(web::resource("/").route(web::get().to(show_register_form)))
@@ -38,10 +40,11 @@ pub fn authorization_routes(config: &mut web::ServiceConfig) {
 // create / delete / modify plants records from the client 
 pub fn plant_routes(config: &mut web::ServiceConfig) {
   config.service(
-    web::scope("/plants")
+  web::scope("/plants")
 //          .service(
 //              web::resource("show/{member_id}/{plant_id}").route(web::get().to(handle_show_member_plant)))
       .service(web::resource("/").route(web::get().to(show_new_plant_form)))
+//      .service(web::resource("/").route(web::get().to(show_register_form)))
       .service(web::resource("add_new").route(web::post().to(new_plant_addition)))
       .service(web::resource("new/{member_id}").route(web::post().to(handle_insert_plant)))
 //          .service(
