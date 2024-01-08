7c81de087fb494e12487f0e4946f458f110a8a72
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-08 15:22:04 +0200

    add handle_delete_plant and coresponding root

--- houseplants-client/src/client_modules/handlers/plant.rs
+++ houseplants-client/src/client_modules/handlers/plant.rs
@@ -23,6 +23,7 @@ pub async fn show_new_plant_form(tmpl: web::Data<tera::Tera>) -> Result<HttpResp
   Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
+// add new plant record , to be used from New Plant Form 
 pub async fn new_plant_addition(
   tmpl: web::Data<tera::Tera>,
   app_state: web::Data<AppState>,
@@ -63,6 +64,8 @@ pub async fn new_plant_addition(
   }
 }
 
+// add new plant record if web::Path is given , 
+// can be used with curl 
 pub async fn handle_insert_plant(
   _tmpl: web::Data<tera::Tera>,
   _app_state: web::Data<AppState>,
@@ -93,4 +96,17 @@ pub async fn handle_insert_plant(
   println!("Finished call: {:?}", res);
   let plant_response: NewPlantResponse = serde_json::from_str(&std::str::from_utf8(&res)?)?;
   Ok(HttpResponse::Ok().json(plant_response))
+}
+
+// delete a plant for a member_id
+pub async fn handle_delete_plant(
+  _tmpl: web::Data<tera::Tera>,
+  _app_state: web::Data<AppState>,
+  path: web::Path<(i32, i32)>
+) -> Result<HttpResponse, Error> {
+  let (member_id, plant_id) = path.into_inner();
+  let awc_client = awc::Client::default();
+  let delete_url = format!("http://localhost:3000/plants/{}/{}", member_id, plant_id);
+  let _res = awc_client.delete(delete_url).send().await.unwrap();
+  Ok(HttpResponse::Ok().body("Plant record deleted "))
 }
