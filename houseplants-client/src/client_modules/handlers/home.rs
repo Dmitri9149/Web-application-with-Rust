5b425a45351c4280c0c9330f234d1c00d27ac306
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-09 18:07:43 +0200

    add navigation block functionality to register.html

--- houseplants-client/src/client_modules/handlers/home.rs
+++ houseplants-client/src/client_modules/handlers/home.rs
@@ -1,11 +1,17 @@
-use actix_web::{get, web, App, HttpServer, HttpResponse};
+use actix_web::{get, web, Error, App, HttpServer, HttpResponse};
 use crate::state::AppState;
 use std::time::{SystemTime, UNIX_EPOCH};
+use crate::errors::CustomError;
 
 // handlers to 'home' scope 
 
-pub async fn index() -> HttpResponse {
-    HttpResponse::Ok().json("Hello World!")
+pub async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+//    HttpResponse::Ok().json("Hello World!")
+    let ctx = tera::Context::new();
+    let s = tmpl
+        .render("home/home.html", &ctx)
+        .map_err(|_| CustomError::TeraError("Template error".to_string()))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
 pub async fn hello(path: web::Path<String>) -> HttpResponse {
