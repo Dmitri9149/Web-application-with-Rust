dc853c05e025690ab44e7778a84e143fd0e120c6
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-13 16:17:47 +0200

    more content to db script

--- houseplants-server/src/server_modules/db_scripts/interesting_facts_plants.sql
+++ houseplants-server/src/server_modules/db_scripts/interesting_facts_plants.sql
@@ -21,13 +21,19 @@ insert into interesting_fact
 (
   fact_id, member_id, content, ref_to_origin, posted_time
 )
-values(1, 1, 'Fact a ', 'Google search', '2023-04-12 05:40:02');
+values(1, 1,
+ '(May be) the oldest pot plant in the world. It is a giant cycad or Encephalartos altensteinii, native to the Eastern Cape province of South Africa and brought to the UK in 1775 by the botanist Francis Masson.',
+ 'Google search',
+ '2023-04-12 05:40:02');
 
 insert into interesting_fact
 (
   fact_id, member_id, content, ref_to_origin, posted_time
 )
-values( 2, 1,'Fact b', 'https://en.wikipedia.org/wiki/List_of_largest_plants', '2023-04-12 05:45:02');
+values( 2, 1,
+  'The largest plant by wood volume and mass is the giant sequoia (Sequoiadendron giganteum), native to Sierra Nevada and California; it grows to an average height of 70–85 m (230–279 ft) and 5–7 m (16–23 ft) in diameter.',
+  'https://en.wikipedia.org/wiki/List_of_largest_plants',
+  '2023-04-12 05:45:02');
 
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO truuser;
