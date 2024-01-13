66a274eb00f8dd1e535022b11f27f741ba75e152
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-13 15:59:50 +0200

    add new sql script to popularte table interesting_fact

--- houseplants-server/src/server_modules/db_scripts/interesting_facts_plants.sql
+++ houseplants-server/src/server_modules/db_scripts/interesting_facts_plants.sql
@@ -5,27 +5,29 @@ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO truuser;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public to truuser;
 GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public to truuser;
 
-drop table if exists interesting_facts cascade;
+drop table if exists interesting_fact cascade;
 
-create table interesting_facts
+create table interesting_fact
 (
-member_id INT not null,
-content varchar(2000) not null,
-ref_to_origin varchar(200),
-posted_time TIMESTAMP default now()
+  fact_id serial primary key,
+  member_id INT not null,
+  content varchar(2000) not null,
+  ref_to_origin varchar(200),
+  posted_time TIMESTAMP default now()
 );
 
 /* Load seed data */ 
-insert into interesting_facts(member_id, content, ref_to_origin)
-values(1,'Methuselah, a Great Basin bristlecone pine (Pinus longaeva) 
-in the White Mountains of California, has been measured by ring count 
-to be 4,855 years old.', 'Google search');
+insert into interesting_fact 
+(
+  fact_id, member_id, content, ref_to_origin, posted_time
+)
+values(1, 1, 'Fact a ', 'Google search', '2023-04-12 05:40:02');
 
-insert into interesting_facts(member_id, content, ref_to_origin)
-values(1,'The largest by wood volume and mass is the giant sequoia 
-(Sequoiadendron giganteum), native to Sierra Nevada and California; 
-it grows to an average height of 70–85 m (230–279 ft) and 5–7 m 
-(16–23 ft) in diameter', 'https://en.wikipedia.org/wiki/List_of_largest_plants');
+insert into interesting_fact
+(
+  fact_id, member_id, content, ref_to_origin, posted_time
+)
+values( 2, 1,'Fact b', 'https://en.wikipedia.org/wiki/List_of_largest_plants', '2023-04-12 05:45:02');
 
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO truuser;
