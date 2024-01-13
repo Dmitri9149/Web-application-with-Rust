6da3cd3e618bd3417ab8e9e54e4649ad668ae0ca
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-13 16:02:46 +0200

    add handlers for interesting_facts

---
+++ houseplants-server/src/server_modules/handlers/interesting_fact.rs
@@ -0,0 +1,12 @@
+use crate::db_access::interesting_fact::*;
+use crate::server_modules::model::interesting_fact::*;
+use crate::state::AppState;
+use actix_web::{web, HttpResponse};
+
+pub async fn get_interesting_facts(
+  app_state: web::Data<AppState>
+) -> HttpResponse {
+    let facts = get_interesting_facts_db(&app_state.db)
+      .await;
+    HttpResponse::Ok().json(facts)
+}
