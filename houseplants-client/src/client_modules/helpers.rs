594a99d2d5c3a222935f5bfc49b05ca99f91b793
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-15 16:58:27 +0200

    change names of functions + delete comments

--- houseplants-client/src/client_modules/helpers.rs
+++ houseplants-client/src/client_modules/helpers.rs
@@ -4,7 +4,6 @@ use argon2::{self, Config};
 
 // get server port from .env file 
 pub fn get_server_port () -> String {
-    // url of resource on server 
     dotenv().ok();
     let port = env::var("SERVER_PORT")
       .expect("Is SERVER_PORT set in .env file? Where in .env file?");
@@ -13,7 +12,6 @@ pub fn get_server_port () -> String {
 
 // get host port from .env file 
 pub fn get_host_port () -> String {
-  // url of resource on server 
   dotenv().ok();
   let port = env::var("HOST_PORT")
     .expect("Is HOST_PORT set in .env file? Where in .env file?");
@@ -21,12 +19,11 @@ pub fn get_host_port () -> String {
 }
 
 // get db url from .env file 
-pub fn get_db_url_client () -> String {
-  // url of resource on server 
+pub fn get_db_url () -> String { 
   dotenv().ok();
-  let db_url_client = env::var("DATABASE_URL")
+  let db_url = env::var("DATABASE_URL")
     .expect("Is DATABASE_URL set in .env file? Where in .env file?");
-  db_url_client
+  db_url
 }
 
 // Hashing the password to store it in DB 
@@ -37,12 +34,3 @@ pub fn hash_password(salt: &str, password: &str) -> String {
     password.as_bytes(), s, &config)
     .unwrap()
 }
-
-/*
-// Hashing the password to store it in DB 
-let salt = b"random_salt";
-let config = Config::default();
-let hash = argon2::hash_encoded(
-  params.password.clone().as_bytes(), salt, &config)
-  .unwrap();
-*/
