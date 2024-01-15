d6038a563e0bb0585e1163ac642d29ed90a34df4
Dmitri9149 <dmitri.as.name@gmail.com>
2024-01-15 16:59:25 +0200

    change name of function

--- houseplants-server/src/server_modules/helpers.rs
+++ houseplants-server/src/server_modules/helpers.rs
@@ -11,10 +11,10 @@ pub fn get_server_port () -> String {
 }
 
 // get server port from .env file 
-pub fn get_db_url_server () -> String {
+pub fn get_db_url () -> String {
   // url of resource on server 
   dotenv().ok();
-  let db_url_client = env::var("DATABASE_URL")
+  let db_url = env::var("DATABASE_URL")
     .expect("Is DATABASE_URL set in .env file? Check where is .env file?");
-  db_url_client
+  db_url
 }
